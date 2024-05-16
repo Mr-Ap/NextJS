c052d09cc7b981afd74c82a743052afa78132e18
Avadhut Patil <Avadhut.Patil@mindbodyonline.com>
2024-05-16 10:35:52 +0530

    feat: setup routes and displayed  news

---
+++ News App/app/news/[slug]/not-found.js
@@ -0,0 +1,8 @@
+export default function NewsFoundPage() {
+  return (
+    <div id="error">
+      <h1>Not Found!</h1>
+      <p>Unfortunately, we could not found this news.</p>
+    </div>
+  );
+}
---
+++ News App/app/news/page.js
@@ -0,0 +1,21 @@
+import { DUMMY_NEWS } from "@/dummy-news";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function NewsPage() {
+  return (
+    <>
+      <h1>NewsPage</h1>
+      <ul className="news-list">
+        {DUMMY_NEWS.map((news) => (
+          <li key={news.id}>
+            <Link href={`/news/${news.slug}`}>
+              <img src={`/images/news/${news.image}`} alt={news.title} />
+              <span>{news.title}</span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
